520aa22c360f929904a3a23a1d23eabd7ebb4d49
Costizaur <168482502+Costizaur@users.noreply.github.com>
2025-05-15 10:02:09 +0200

    Update AltarBehaviour.cs

--- Assets/Scripts/AltarBehaviour.cs
+++ Assets/Scripts/AltarBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,38 @@ public class AltarBehaviour : MonoBehaviour
     [HideInInspector] public GameObject item;
     private AudioSource _audio;
     private GameObject _currentItem;
+
+    private GameObject player = null;
+    private InventoryManager inventoryManager;
     void Awake()
     {
         _audio = GetComponent<AudioSource>();
-        // Ensure the collider is a trigger for detection
-        var col = GetComponent<Collider>();
-        col.isTrigger = true;
-        // Tag this object as "Altar" so items can detect it
-        gameObject.tag = "Altar";
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        inventoryManager = InventoryManager.Instance;
     }
 
+    private void Update()
+    {
+        PauseSoundWhenInRange();
+    }
+
+    private void PauseSoundWhenInRange()
+    {
+        if (player == null || inventoryManager == null) return;
+        
+        float distance = Vector3.Distance(player.transform.position, inventoryManager.gameObject.transform.position);
+        if (distance <= inventoryManager.interactionDistance)
+        {
+            _audio.Pause();
+        }
+        else if (!_audio.isPlaying)
+        {
+            _audio.Play();
+        }
+    }
+    
+    
     /// <summary>
     /// Called by the item when it enters/exits the altar trigger.
     /// </summary>
