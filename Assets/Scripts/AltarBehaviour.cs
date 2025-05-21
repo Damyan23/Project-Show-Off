8d821501b3da5c056217f7b7e8bda9a699268e0e
Damyan23 <d.peychev12@gmail.com>
2025-05-21 15:27:32 +0200

    Some small changes
    Added ui for the current task and made a class with a helper function to be able to change the current task, also added a class to be able to change the fire particle effect's life over some time. Also looked a bit into how to work with yarnspinner and tried to fix the glow for the items

--- Assets/Scripts/AltarBehaviour.cs
+++ Assets/Scripts/AltarBehaviour.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AltarBehaviour : MonoBehaviour
 {
+    [Header("Altart settings")]
     [HideInInspector] public bool isSlotTaken = false;
     [HideInInspector] public GameObject item;
 
+    [Header("Sound settings")]
     [SerializeField] private float intervalInSeconds = 5f;
-
     [SerializeField] private AudioClip _clip;
-    private AudioSource _audio;
+    private AudioSource audioSource;
+    private float timeSinceLastSound = 0f;
 
-    private GameObject _currentItem;
+    [Header("References")]
+    private GameObject _currentItem = null;
     private GameObject player = null;
     private InventoryManager inventoryManager;
 
-    private float timeSinceLastSound = 0f;
+    [Header("Particles settings")]
+    private ParticleSystem[] particles;
 
     void Awake()
     {
-        _audio = GetComponent<AudioSource>();
+        TryGetComponent<AudioSource>(out audioSource);
 
         player = GameObject.FindWithTag("Player");
         if (player == null)
@@ -29,27 +34,28 @@ public class AltarBehaviour : MonoBehaviour
         }
 
         inventoryManager = InventoryManager.Instance;
+        particles = GetComponentsInChildren<ParticleSystem>();
     }
 
     private void Update()
     {
-        if (isSlotTaken)
+        if (isSlotTaken && audioSource != null)
         {
-            if (_audio.isPlaying) _audio.Pause();
+            if (audioSource.isPlaying) audioSource.Pause();
             return;
         }
 
         float distance = Vector3.Distance(player.transform.position, transform.position);
         if (distance <= inventoryManager.interactionDistance)
         {
-            if (_audio.isPlaying) _audio.Pause();
+            if (audioSource.isPlaying) audioSource.Pause();
             return;
         }
 
         timeSinceLastSound += Time.deltaTime;
         if (timeSinceLastSound >= intervalInSeconds && _clip != null)
         {
-            _audio.PlayOneShot(_clip);
+            audioSource.PlayOneShot(_clip);
             timeSinceLastSound = 0f;
         }
     }
@@ -60,5 +66,16 @@ public class AltarBehaviour : MonoBehaviour
         isSlotTaken = true;
         _currentItem = item;
         item.transform.SetParent(transform);
+        lightCandelsUp();
+    }
+
+    void lightCandelsUp()
+    {
+        if (particles.Length == 0) return;
+
+        foreach (var particle in particles)
+        {
+            if (!particle.isPlaying) particle.Play();
+        }
     }
 }
