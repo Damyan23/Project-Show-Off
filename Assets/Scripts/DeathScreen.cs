352c85bb0ee48764a72fa2c90e3c6894b99e5719
531374 <531374@student.saxion.nl>
2025-06-10 15:55:57 +0200

    Death Scene Prototype
    Simple death scene transition

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DeathScreen : MonoBehaviour
+{
+    [SerializeField] private Image blackFade;
+
+    [SerializeField] private Transform enemy;
+    [SerializeField] private float enemyMoveSpeed;
+    [SerializeField] private float fogIntensity = 5f;
+
+    private bool transitionDone = false;
+    private Volume globalVolume;
+
+    private Vector3 enemyMoveDir;
+   
+    private void Start()
+    {
+        Fog fog;
+        globalVolume = GameObject.Find("Global Volume").GetComponent<Volume>();
+        globalVolume.profile.TryGet(out fog);
+        fog.meanFreePath.value = fogIntensity;
+
+        enemyMoveDir = Vector3.Normalize(Camera.main.transform.position - enemy.position);
+        enemy.rotation = Quaternion.LookRotation(-enemyMoveDir);
+
+        StartCoroutine(TransitionIntoScene());
+    }
+
+    private void Update()
+    {
+        if (!transitionDone) return;
+
+        //Make enemy move faster over time
+        //enemyMoveSpeed = Mathf.Min(enemyMoveSpeed * 1.05f, 15f);
+        float frameDst = Time.deltaTime * enemyMoveSpeed;   
+
+        if(Vector3.Distance(enemy.position, Camera.main.transform.position) > frameDst)
+        {
+
+            enemy.Translate(enemyMoveDir * frameDst);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private IEnumerator TransitionIntoScene()
+    {
+        float alpha = 1f;
+
+        while (alpha > 0f)
+        {
+            alpha -= 0.05f;
+            blackFade.color = new Color(0f, 0f, 0f, alpha);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        transitionDone = true;
+    }
+}
