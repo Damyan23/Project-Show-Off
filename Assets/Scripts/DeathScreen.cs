b7e50d43918c2c2e6212c6374d1ad7f1d17b5f1e
531374 <531374@student.saxion.nl>
2025-06-11 11:05:53 +0200

    Added Transition to Main Menu
    Death screen now fades into main menu
    Bug Fixes

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -12,11 +12,11 @@ public class DeathScreen : MonoBehaviour
 
     [SerializeField] private Transform enemy;
     [SerializeField] private float enemyMoveSpeed;
-    [SerializeField] private float fogIntensity = 5f;
+    [SerializeField, Tooltip("Lower Value = More Fog")] private float fogIntensity = 5f;
 
     private bool transitionDone = false;
     private Volume globalVolume;
-
+    private Transform camT;
     private Vector3 enemyMoveDir;
    
     private void Start()
@@ -26,7 +26,9 @@ public class DeathScreen : MonoBehaviour
         globalVolume.profile.TryGet(out fog);
         fog.meanFreePath.value = fogIntensity;
 
-        enemyMoveDir = Vector3.Normalize(Camera.main.transform.position - enemy.position);
+        camT = Camera.main.transform;
+
+        enemyMoveDir = Vector3.Normalize(camT.position - enemy.position);
         enemy.rotation = Quaternion.LookRotation(-enemyMoveDir);
 
         StartCoroutine(TransitionIntoScene());
@@ -40,14 +42,13 @@ public class DeathScreen : MonoBehaviour
         //enemyMoveSpeed = Mathf.Min(enemyMoveSpeed * 1.05f, 15f);
         float frameDst = Time.deltaTime * enemyMoveSpeed;   
 
-        if(Vector3.Distance(enemy.position, Camera.main.transform.position) > frameDst)
+        if(Vector3.Distance(enemy.position, camT.position) > frameDst)
         {
-
             enemy.Translate(enemyMoveDir * frameDst);
         }
         else
         {
-            SceneManager.LoadScene(0);
+            StartCoroutine(EndGame());
         }
     }
 
@@ -65,5 +66,14 @@ public class DeathScreen : MonoBehaviour
         yield return new WaitForSeconds(1f);
 
         transitionDone = true;
+    }
+
+    private IEnumerator EndGame()
+    {
+        blackFade.color = new Color(0f, 0f, 0f, 1f);
+
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene(0);
     }
 }
