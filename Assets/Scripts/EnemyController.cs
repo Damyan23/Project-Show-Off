05de3e625cbdcab6eea418fc35a4ddb63e9c8214
Costizaur <168482502+Costizaur@users.noreply.github.com>
2025-06-19 11:46:47 +0200

    Squashed commit of the following:
    commit e1776f1d196c857525418195c65ee62692b1c11b
    Merge: e94fae9 44f7595
    Author: Damyan23 <d.peychev12@gmail.com>
    Date:   Thu Jun 19 11:35:14 2025 +0200
    
        Merge branch 'idk' into Designer-Branch-Fixed
    
    commit e94fae9b089900f41dc724f758f3026b0254841a
    Merge: 4354845 cc2f7cf
    Author: Damyan23 <d.peychev12@gmail.com>
    Date:   Thu Jun 19 11:15:35 2025 +0200
    
        Merge branch 'branch' into Designer-Branch-Fixed
    
    commit 43548452a5e70a58dcf5f07b33fdc334e652f0dd
    Author: Damyan23 <d.peychev12@gmail.com>
    Date:   Thu Jun 19 11:11:26 2025 +0200
    
        IDK
    
    commit 0deec295ffb82fe3fdf3971afe6b674f6b68512b
    Author: 531374 <531374@student.saxion.nl>
    Date:   Thu Jun 19 10:48:08 2025 +0200
    
        Enemy look fix
    
        Enemy will not look up or down anymore
    
    commit dadd88a93947b680a597857cdc7ea939e28706b6
    Merge: 7a93eef 56abe9e
    Author: Costizaur <168482502+Costizaur@users.noreply.github.com>
    Date:   Thu Jun 19 10:08:02 2025 +0200
    
        Merge branch 'fix' of https://github.com/Damyan23/Project-Show-Off into fix
    
    commit 7a93eefc84a075900b915d0a38a60cbb888498f8
    Author: Costizaur <168482502+Costizaur@users.noreply.github.com>
    Date:   Thu Jun 19 10:07:04 2025 +0200
    
        sounds and offerings

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -41,6 +41,7 @@ public class EnemyController : MonoBehaviour
             //Move toward player
             Vector3 dirToPlayer = Vector3.Normalize(playerT.position - transform.position);
             transform.Translate(chaseSpeed * Time.deltaTime * dirToPlayer, Space.World);
+            dirToPlayer.y = 0f;
             transform.rotation = Quaternion.LookRotation(dirToPlayer);
         }
         else
@@ -50,6 +51,7 @@ public class EnemyController : MonoBehaviour
             //Move toward next point
             Vector3 dirToNextPoint = Vector3.Normalize(points[currentPointIndex] - transform.position);
             transform.Translate(roamingSpeed * Time.deltaTime * dirToNextPoint, Space.World);
+            dirToNextPoint.y = 0f;
             transform.rotation = Quaternion.LookRotation(dirToNextPoint);
 
             if(Vector3.Distance(transform.position, points[currentPointIndex]) < 0.1f)
