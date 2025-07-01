cbf3567ef09ed0cff32eb115b5f4412a8f6c07ce
Damyan23 <d.peychev12@gmail.com>
2025-07-01 11:57:58 +0200

    Altar Interaction

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     #region Singleton
-        public static GameManager instance;
+    public static GameManager instance;
 
     void Awake()
     {
@@ -26,7 +26,7 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
-
+    private Transform playerTransform;
     [Header("Game over Settings")]
     [SerializeField] private int requiredNumberOfDoneAltars = 3;
     [HideInInspector] public int numberOfDoneAltars = 0;
@@ -45,12 +45,21 @@ public class GameManager : MonoBehaviour
     // Create an instance of the delegate that other scripts can subscribe to
     public OnAltarDoneEvent onAltarDone;
 
+    [Header("White Woman Spawn Settings")]
+    [SerializeField] private GameObject whiteWomanPrefab;
+    [SerializeField] private float minDistance = 8f;
+    [SerializeField] private float maxDistance = 15f;
+    [SerializeField] private float ariseHeight = 2f;
+    [SerializeField] private float ariseSpeed = 2f;
+
+
     void Start()
     {
-        taskManager.UpdateTask (" Find an item and pick it up");    
+        taskManager.UpdateTask(" Find an item and pick it up");
         onAltarDone += IncrementAltarCount;
         cribAudioSource = crib.AddComponent<AudioSource>();
         crib.GetComponent<AudioSource>().clip = babySpawnedSound;
+        playerTransform = GameObject.FindWithTag("Player").transform;
     }
 
     void Update()
@@ -66,7 +75,7 @@ public class GameManager : MonoBehaviour
         crib.GetComponent<AudioSource>().Play();
         babySpawned = true;
     }
-    
+
     private void IncrementAltarCount()
     {
         numberOfDoneAltars++;
@@ -76,7 +85,7 @@ public class GameManager : MonoBehaviour
 
         Debug.Log("Altar completed! Total: " + numberOfDoneAltars + "/" + requiredNumberOfDoneAltars);
     }
-    
+
     public void AltarCompleted()
     {
         // Check if anyone is subscribed to the event before invoking
@@ -84,5 +93,47 @@ public class GameManager : MonoBehaviour
         {
             onAltarDone.Invoke();
         }
+    }
+    
+    public void SpawnWhiteWoman()
+    {
+        if (whiteWomanPrefab != null)
+        {
+            // Random angle: -45° (left) to +45° (right) relative to forward
+            float angle = Random.Range(-45f, 45f);
+            float distance = Random.Range(minDistance, maxDistance);
+
+            // Direction vector
+            Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+            Vector3 spawnPos = playerTransform.position + direction.normalized * distance;
+
+            // Spawn below ground
+            spawnPos.y -= ariseHeight;
+
+            GameObject ww = Instantiate(whiteWomanPrefab, spawnPos, Quaternion.identity, this.transform);
+
+            // Start coroutine to move up
+            StartCoroutine(AriseWhiteWoman(ww, ariseHeight));
+        }
+        else
+        {
+            Debug.LogError("White woman prefab is not assigned in PlayerInteraction.");
+        }
+    }
+
+    private IEnumerator AriseWhiteWoman(GameObject ww, float height)
+    {
+        Vector3 start = ww.transform.position;
+        Vector3 end = start + Vector3.up * height;
+        float elapsed = 0f;
+        float duration = height / ariseSpeed;
+
+        while (elapsed < duration)
+        {
+            ww.transform.position = Vector3.Lerp(start, end, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        ww.transform.position = end;
     }
 }
